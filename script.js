7f05529c9fcd7202a9fe044c4c894ea3f5f27a88
Ajay Singh Kunwar <ajaykunwar323@gmail.com>
2024-10-16 17:20:21 +0530

    add third page

--- script.js
+++ script.js
@@ -47,4 +47,62 @@ nav.addEventListener("mouseleave",function(){
 })
 }
 
+function page2Animation(){
+    var rightElems=  document.querySelectorAll(".right-elem");
+    rightElems.forEach((elem)=>{
+        elem.addEventListener("mouseenter",()=>{
+            gsap.to(elem.childNodes[3],{
+                opacity: 1,
+                scale : 1
+            })
+            
+        })
+        elem.addEventListener("mouseleave",()=>{
+            gsap.to(elem.childNodes[3],{
+                opacity: 0,
+                scale : 0
+            })
+        })
+        elem.addEventListener("mousemove",(dets)=>{
+            console.log(elem.getBoundingClientRect().y)
+            gsap.to(elem.childNodes[3],{
+                x:dets.x-elem.getBoundingClientRect().x-88,
+                y:dets.y-elem.getBoundingClientRect().y-230
+            })
+        })
+    })
+}
+
+function page3VideoAnimation(){
+
+
+    var page3Center= document.querySelector(".page3-center");
+    var video= document.querySelector("#page3 video");
+    
+    page3Center.addEventListener("click",()=>{
+        video.play();
+        gsap.to(video,{
+            transform : "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius : 0,
+            transition: "all ease 0.9s"
+        })
+    })
+    
+    video.addEventListener("click",()=>{
+        video.pause();
+        gsap.to(video,{
+            opacity: 0,
+            borderRadius : "35px",
+            transform : "scaleX(0.7) scaleY(0)"
+        })
+    })
+    
+    
+
+}
+
+
+page3VideoAnimation()
 navAnimation()
+page2Animation();
